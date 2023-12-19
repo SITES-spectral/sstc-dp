9a27b642aa4bca0d8181d13f5aea09b8f3e9ea97
Jobelund <124563223+jobelund@users.noreply.github.com>
2023-12-19 20:03:44 +0100

    v0.1.1 added function

--- src/sstc_dp/__init__.py
+++ src/sstc_dp/__init__.py
@@ -1,5 +1,8 @@
 import icoscp_core.sites as sites
 from typing import List, Dict, Union
+import geopandas as gpd
+import pandas as pd
+from shapely import Point
 
 
 def get_sites_stations_list(inactive_stations_ids=["TRS"])->List(Dict[str, Union[str, int, float]]):
@@ -55,3 +58,56 @@ def get_sites_stations_list(inactive_stations_ids=["TRS"])->List(Dict[str, Union
             continue  # Skip this station and continue with the next one
 
     return stations
+
+
+def get_stations_geodataframe(epsg=4326):
+    """
+    Retrieves station data, converts it into a GeoDataFrame, and sets the specified coordinate reference system (CRS).
+
+    The function first calls 'get_sites_stations_list' to retrieve station data. This data is then converted into 
+    a pandas DataFrame. Each station's longitude and latitude are used to create a 'Point' geometry, which is necessary 
+    for geospatial analysis. Finally, the DataFrame is converted into a GeoDataFrame, and the CRS is set based on 
+    the given EPSG code.
+
+    Args:
+        epsg (int, optional): The EPSG code for the desired coordinate reference system. Default is 4326 
+            (WGS 84 - World Geodetic System 1984).
+
+    Returns:
+        geopandas.GeoDataFrame: A GeoDataFrame containing station data with geometry and specified CRS.
+
+    Raises:
+        ImportError: If required modules (pandas, geopandas, shapely) are not installed.
+        Exception: For any other unforeseen errors during the function execution.
+
+    Example:
+        >>> gdf = get_stations_geodataframe(epsg=4326)
+        >>> print(gdf.head())
+
+    Note:
+        - This function depends on 'get_sites_stations_list', 'pandas', 'geopandas', and 'shapely' being available.
+        - It's important to ensure the EPSG code provided is valid and appropriate for the intended geospatial analysis.
+    """
+    from shapely.geometry import Point
+    
+    try:
+        # Retrieve station data using the get_sites_stations_list function
+        stations = get_sites_stations_list()
+
+        # Convert the list of station data into a pandas DataFrame
+        df = pd.DataFrame(stations)
+
+        # Create a Point geometry for each station
+        df['geometry'] = df.apply(lambda row: Point(row['longitude'], row['latitude']), axis=1)
+
+        # Convert to GeoDataFrame and set CRS
+        gdf = gpd.GeoDataFrame(df, geometry='geometry')
+        gdf.set_crs(epsg=epsg, inplace=True)
+
+        return gdf
+    except ImportError as ie:
+        raise ImportError(f"Missing required module: {ie}")
+    except Exception as e:
+        raise Exception(f"An error occurred in get_stations_geodataframe: {e}")
+
+
